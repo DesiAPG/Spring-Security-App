854dc22b494b98921f6860bc8f4d6dbd1d62a6c1
Desireeee <74121435+DesiAPG@users.noreply.github.com>
2023-07-21 16:29:32 -0500

    Permissions, sessions, basic auth and configurations
    Learn how to manage permissions and session time, also about basic auth to send authorization through the headers

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -2,9 +2,15 @@ package com.example.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -17,8 +23,28 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest()
                         .authenticated())
+                .httpBasic(Customizer.withDefaults())
                 .formLogin(formLogin -> formLogin
+                        .successHandler(successHandler()) //Url to redirect after login
                         .permitAll())
+                .sessionManagement(sessionManager -> sessionManager
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                        .invalidSessionUrl("/login")
+                        .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::migrateSession)
+                        .maximumSessions(1)
+                        .expiredUrl("/login")
+                        .sessionRegistry(sessionRegistry()))
                 .build();
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    public AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+            response.sendRedirect("/v1/session");
+        });
     }
 }
--- src/main/java/com/example/springsecurity/controller/CustomerController.java
+++ src/main/java/com/example/springsecurity/controller/CustomerController.java
@@ -1,13 +1,25 @@
 package com.example.springsecurity.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("v1")
 public class CustomerController {
+    @Autowired
+    private SessionRegistry sessionRegistry;
+
     @GetMapping("/index")
     public String index() {
         return "Secured";
@@ -16,5 +28,29 @@ public class CustomerController {
     @GetMapping("/index2")
     public String index2() {
         return "NOT SECURED";
+    }
+
+    @GetMapping("/session")
+    public ResponseEntity<?> getSessionDetails() {
+        String sessionId = "";
+        User userObject = null;
+
+        List<Object> sessions = sessionRegistry.getAllPrincipals();
+
+        for (Object session : sessions) {
+            if (session instanceof User) {
+                userObject = (User) session;
+            }
+            List<SessionInformation> sessionInformations = sessionRegistry.getAllSessions(session, false);
+            for (SessionInformation sessionInformation : sessionInformations) {
+                sessionId = sessionInformation.getSessionId();
+            }
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("response", "Hello world");
+        response.put("sessionUser", userObject);
+        response.put("sessionId", sessionId);
+        return ResponseEntity.ok(response);
     }
 }
